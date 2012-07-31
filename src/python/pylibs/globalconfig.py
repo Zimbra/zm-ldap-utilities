d6e4caac64f8e976503ee6b6c058e672e57da0b4
Vishal Mahajan <vmahajan@zimbra.com>
2012-07-31 05:27:01 -0500

    bug: 76500 - HTML responses sent with prefix do not show with any prefix / blue bar
    - Revised the regular expression used for stripping off all css functions other than rgb.
    - Added a unit test.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=76500
    
    Copied from Perforce
     Change: 389572

--- src/python/pylibs/globalconfig.py
+++ src/python/pylibs/globalconfig.py
@@ -1,13 +1,13 @@
 #
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2010, 2011 VMware, Inc.
-# 
+# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+#
 # The contents of this file are subject to the Zimbra Public License
 # Version 1.3 ("License"); you may not use this file except in
 # compliance with the License.  You may obtain a copy of the License at
 # http://www.zimbra.com/license.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 # ***** END LICENSE BLOCK *****
@@ -60,15 +60,25 @@ class GlobalConfig(config.Config):
 			self["zimbraMtaRestrictionRBLs"] = ' '.join(p)
 
 		if self["zimbraIPMode"] is not None:
+			self["zimbraIPv4BindAddress"] = "127.0.0.1"
 			v = self["zimbraIPMode"]
 			v = str(v)
 			v = v.lower()
 			if v == "ipv4":
+				self["zimbraLocalBindAddress"] = "127.0.0.1"
 				self["zimbraPostconfProtocol"] = "ipv4"
+				self["zimbraAmavisListenSockets"] = "'10024','10026'"
+				self["zimbraInetMode"] = "inet"
 			if v == "ipv6":
+				self["zimbraLocalBindAddress"] = "::1"
 				self["zimbraPostconfProtocol"] = "ipv6"
+				self["zimbraAmavisListenSockets"] = "'[::1]:10024','[::1]:10026'"
+				self["zimbraInetMode"] = "inet6"
 			if v == "both":
+				self["zimbraLocalBindAddress"] = "::1"
 				self["zimbraPostconfProtocol"] = "all"
+				self["zimbraAmavisListenSockets"] = "'10024','10026','[::1]:10024','[::1]:10026'"
+				self["zimbraInetMode"] = "inet6"
 
 		dt = time.clock()-t1
 		Log.logMsg(5,"globalconfig loaded in %.2f seconds" % dt)
--- src/python/pylibs/logmsg.py
+++ src/python/pylibs/logmsg.py
@@ -1,13 +1,13 @@
 #
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2010, 2011 VMware, Inc.
-# 
+# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+#
 # The contents of this file are subject to the Zimbra Public License
 # Version 1.3 ("License"); you may not use this file except in
 # compliance with the License.  You may obtain a copy of the License at
 # http://www.zimbra.com/license.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 # ***** END LICENSE BLOCK *****
@@ -23,18 +23,16 @@ import logging.handlers
 import re
 import threading
 
-lvlmap = {
-	5	: logging.DEBUG,
-	4	: logging.INFO,
-	3	: logging.WARNING,
-	2	: logging.ERROR,
-	1	: logging.CRITICAL,
-	0	: logging.FATAL,
-	}
+from org.productivity.java.syslog4j import Syslog
+from org.productivity.java.syslog4j import SyslogIF
+from org.productivity.java.syslog4j import SyslogConstants
+from org.productivity.java.syslog4j.impl.unix import UnixSyslog
+from org.productivity.java.syslog4j.impl.unix.socket import UnixSocketSyslogConfig
 
 class Log:
-	loghandler = None
-	sysloghandler = None
+	zmconfigdSyslogInstance = UnixSocketSyslogConfig(SyslogConstants.FACILITY_LOCAL0, "/dev/log")
+	zmsyslog = Syslog.createInstance("zmSyslog",zmconfigdSyslogInstance)
+	zmsyslog.getConfig().setLocalName("zmconfigd[%d]:" % os.getpid())
 
 	@classmethod
 	def initLogging(cls, c = None):
@@ -45,37 +43,18 @@ class Log:
 		else:
 			cls.cf = conf.Config()
 
-		fmt = logging.Formatter("%(asctime)s %(name)s %(levelname)s [%(process)d-%(threadName)s] %(message)s")
-		sfmt = logging.Formatter("%(name)s %(levelname)s [%(process)d-%(threadName)s-%(thread)d] %(message)s")
-
-		cls.logger = logging.getLogger('zmconfigd')
-		cls.logger.setLevel(logging.DEBUG)
-		if (cls.loghandler):
-			cls.logger.removeHandler(cls.loghandler)
-		cls.loghandler = logging.handlers.RotatingFileHandler("/opt/zimbra/log/zmconfigd.log",maxBytes=10000000,backupCount=5)
-		cls.loghandler.setFormatter(fmt)
-		cls.loghandler.setLevel(lvlmap[cls.cf.loglevel])
-		cls.logger.addHandler(cls.loghandler)
-
-		if (cls.sysloghandler):
-			cls.logger.removeHandler(cls.sysloghandler)
-		cls.sysloghandler = logging.handlers.SysLogHandler(('localhost',514),logging.handlers.SysLogHandler.LOG_LOCAL0)
-		cls.sysloghandler.setFormatter(sfmt)
-		cls.sysloghandler.setLevel(logging.CRITICAL)
-
-		cls.logger.addHandler(cls.sysloghandler)
-
 	@classmethod
 	def logMsg(cls, lvl, msg):
 
 		if lvl > 5:
 			lvl = 5
 		msg = re.sub(r"\s|\n", " ", msg)
-		# print "Logging at %d (%d)" % (lvl, lvlmap[lvl])
-		cls.logger.log( lvlmap[lvl], msg) 
+
+		if lvl <= cls.cf.loglevel:
+			Log.zmsyslog.log(lvl, msg)
 
 		if lvl == 0:
-			cls.logger.log( lvlmap[lvl], "%s: shutting down" % (cls.cf.progname,) )
+			Log.zmsyslog.log(2, "%s: shutting down" % (cls.cf.progname,) )
 			os._exit(1)
 
 Log.initLogging()
